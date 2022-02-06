6094d5526396a0895df1e040e8fbe36fe36be32e
Dominikcos7 <Dominikcos@gmail.com>
2022-02-06 12:59:12 +0100

    made header and navbar
    added logo to header, redone navbar and added animation to navbar

--- header.php
+++ header.php
@@ -8,21 +8,31 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');</style>
     <link rel="stylesheet" href="style.css">
     <title>Főoldal</title>
 </head>
 <body>
-    <div class="header">
-        <div class="header-right">
-            <a href="index.php">Főoldal</a> 
-            <?php
-                if(isset($_SESSION["usersEmail"])){
-                    echo'<a href="profile.php">Profil</a>';
-                    echo'<a href="includes/logout.inc.php">Kijelentkezés</a>';
-                }else{
-                    echo"<a href='login.php'>Bejelentkezés</a>";
-                    echo"<a href='register.php'>Regisztráció</a>";
-                }
-            ?>            
+    <header>
+        <div class="container">
+            <img src="img/pngegg (1).png" class="logo"> 
+
+            <nav>
+                <ul>
+                    <li><a href="index.php">Főoldal</a></li>
+                    <li><?php 
+                            if(isset($_SESSION["usersEmail"]))
+                            echo'<a href="profile.php">Profil</a>';
+                            else
+                            echo'<a href="register.php">Regisztráció</a>';
+                    ?></li>
+                    <li><?php 
+                            if(isset($_SESSION["usersEmail"]))
+                            echo'<a href="includes/logout.inc.php">Kijelentkezés</a>';
+                            else
+                            echo'<a href="login.php">Bejelentkezés</a>';
+                    ?></li>
+                </ul>
+            </nav>
         </div>
-    </div>
+    </header>
