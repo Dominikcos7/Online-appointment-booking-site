1dfc97221d97f7becc7a82829dd2d01e269c3968
Dominikcos7 <Dominikcos@gmail.com>
2022-02-06 15:44:09 +0100

    styled login and register pages
    centered wrapper, gave it a different colour, styled input fields and submit button, also styled error messages

--- register.php
+++ register.php
@@ -2,7 +2,8 @@
 include_once 'header.php';
 ?>
 
-<section class="signup-form">
+
+<div class="forms-wrapper">
     <h1>Regisztráció</h1>
     <form action="includes/register.inc.php" method="post"><br>
         <input type="text" name="fullname" placeholder="Teljes név..."><br>
@@ -10,33 +11,31 @@ include_once 'header.php';
         <input type="text" name="telnumber" placeholder="Telefonszám...(pl: 06/30-123-4567)"><br>
         <input type="password" name="password" placeholder="Jelszó..."><br>
         <input type="password" name="passwordAgain" placeholder="Jelszó megint..."><br>
-        <button type="submit" name="submit">Regisztráció</button>
+        <button type="submit" name="submit" id="submit">Regisztráció</button>
     </form>
-    <?php
+<?php
 if(isset($_GET["error"])){
     if($_GET["error"]=="emptyinput"){
-        echo'<script type="text/javascript">window.alert("Tölts ki minden mezőt!");</script>';
+        echo'<p class="errormessage">Nem töltöttél ki minden mezőt!</p>';
     }
-    else if($_GET["error"]=="invalidusername"){
-        echo'<script type="text/javascript">window.alert("Adj meg egy valódi felhasználónevet!");</script>';
-    }
-    else if($_GET["error"]=="invalidemail"){
-        echo'<script type="text/javascript">window.alert("Adj meg egy valódi emailt!");</script>';
+    else if($_GET["error"]=="invalidemail"){        
+        echo'<p class="errormessage">Rossz emailt adtál meg!</p>';
     }
     else if($_GET["error"]=="passnotmatching"){
-        echo'<script type="text/javascript">window.alert("A jelszavak nem egyeznek meg!");</script>';
+        echo'<p class="errormessage">A jelszavak nem egyeznek meg!</p>';
     }
     else if($_GET["error"]=="stmtfailed"){
-        echo'<script type="text/javascript">window.alert("Valami félresikerült...");</script>';
+        echo'<p class="errormessage">Ezzel az emaillel már regisztráltak!</p>';
     }else if($_GET["error"]=="emailtaken"){
-        echo'<script type="text/javascript">window.alert("Ezzel az emaillel már regisztráltak!");</script>';
+        echo'<p class="errormessage">Ezzel az emaillel már regisztráltak!</p>';
     }
     else if($_GET["error"]=="none"){
-        echo'<script type="text/javascript">window.alert("Sikeres regisztráció!");</script>';
+        echo'<p class="errormessage">Sikeres regisztráció!</p>';
     }
 }
 ?>
-</section>
+</div>
+
 
 
 
